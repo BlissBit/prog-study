78dabaa3c22a951ed1616a97f33fca8182fac28a
amatyushov <matyushov.a.a@gmail.com>
2021-02-08 00:59:57 +0300

    add function mutip

--- amatyushov/example-functions/src/main.cpp
+++ amatyushov/example-functions/src/main.cpp
@@ -4,9 +4,13 @@ int sum(int a, int b)
 {
 	return a + b;
 }
-
+int multyp(int a, int b)
+{
+	return a * b;
+}
 int main()
 {
 	std::cout << sum(2,2) << std::endl;
+	std::cout << multyp(2, 2) << std::endl;
 	return 0;
 }
