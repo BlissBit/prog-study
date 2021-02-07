06aa4d839125f7bdc422a54605e52591d29d81d3
Alexandr <matyushov.a.a@gmail.com>
2021-02-07 05:02:40 +0300

    add example switch (#1)
    add example switch

---
+++ amatyushov/example-switch/src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+enum types
+{
+	first,
+	second,
+	third
+};
+
+int main()
+{
+	std::cout << "switch example" << std::endl;
+	const types num = types::first;
+	switch (num)
+	{
+		case types::first:
+		{
+			 std::cout << "types::first" << std::endl;
+		}
+		break;
+		case types::second:
+		{
+			std::cout << "types::second" << std::endl;
+		}
+		break;
+		case types::third:
+		{
+			std::cout << "" << std::endl;
+		}
+		break;
+		default:
+		{
+			std::cout << "types::third" << std::endl;
+		}
+		break;
+	}
+	return 0;
+}
