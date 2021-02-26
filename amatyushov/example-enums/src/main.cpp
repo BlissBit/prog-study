1e83587543412b752a88c3070eadc6c191eb7f66
Alexandr <matyushov.a.a@gmail.com>
2021-02-27 02:04:16 +0300

    add enum example (#8)

--- amatyushov/example-enums/src/main.cpp
+++ amatyushov/example-enums/src/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 
+enum ActionType {
+	Addition,
+	Multiplication,
+	Subtraction,
+	Division
+};
+
 int main()
 {
-	std::cout << "Endl" << std::endl;
+	int A, B, action= 0;
+	std::cout << "Enter A: ";
+	std::cin >> A;
+	std::cout << "Enter B: ";
+	std::cin >> B;
+
+	std::cout << "Enter action (1-Addition, 2-Multiplication, 3-Subtraction, 4-Division):";
+	std::cin >> action;
+
+	switch (action)
+	{
+		case ActionType::Addition:
+		{
+			std::cout << "Result: " << A + B << std::endl;
+		}
+		break;
+		case ActionType::Multiplication:
+		{
+			std::cout << "Result: " << A * B << std::endl;
+		}
+		break;
+		case ActionType::Subtraction:
+		{
+			std::cout << "Result: " << A - B << std::endl;
+		}
+		break;
+		case ActionType::Division:
+		{
+			std::cout << "Result: " << A / B << std::endl;
+		}
+		break;
+	}
+
 	return 0;
 }
