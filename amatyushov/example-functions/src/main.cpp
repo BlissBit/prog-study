eefbdffc42509a3de4013556fcc9f6e48f85ef27
Alexandr <matyushov.a.a@gmail.com>
2021-02-26 22:36:57 +0300

    add example functions (#7)

--- amatyushov/example-functions/src/main.cpp
+++ amatyushov/example-functions/src/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 
+bool fun(int a, int b)
+{
+	return a > b;
+}
+
+int sum(int a, int b)
+{
+	return a + b;
+}
+
 int main()
 {
-	std::cout << "funk" << std::endl;
+
+	int A, B = 0;
+	std::cout << "Enter A: ";
+	std::cin >> A;
+	std::cout << "Enter B: ";
+	std::cin >> B;
+
+	
+	if (fun(A, B))
+	{
+		std::cout << "A > B" << std::endl;
+	}
+	else
+	{
+		std::cout << "A <= B" << std::endl;
+	}
+	
+	std::cout << "A + B = " << sum(A, B) << std::endl;
+
 	return 0;
 }
