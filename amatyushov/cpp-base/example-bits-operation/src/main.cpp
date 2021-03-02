d7e0872101767745964c1eceae1a9b3916b51e21
matyushov.a.a@gmail.com <matyushov.a.a@gmail.com>
2021-03-02 20:46:31 +0300

    add bits operation

---
+++ amatyushov/cpp-base/example-bits-operation/src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	// int x = 0;
+	// std::cout << "Enter x: ";
+	// std::cin >> x;
+
+	/*Bits operation*/
+	unsigned int a = 5;
+	a = a << 1; // a = 10
+	a = a >> 1; // a = 5
+
+	a = 1;
+	a = ~a;
+	int x = 5;
+	int y = 6;
+
+	a = x & y;
+	a = x | y;
+
+	// <<=
+	// >>=
+	// |=
+	// &=
+	// ^=
+
+	return 0;
+}
