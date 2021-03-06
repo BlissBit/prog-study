28afbca37c4fa32d6dbe8ead1e00fa8608bcf07a
DShpilkA <darkangel77792.e30@gmail.com>
2021-03-07 01:58:37 +0300

    [sp-11] подключил библиотеку cmath

--- dshpilka/sp-11/src/main.cpp
+++ dshpilka/sp-11/src/main.cpp
@@ -1,23 +1,32 @@
-#include<iostream>
+#include <iostream>
+#include <cmath>
 
-int main() 
+int main()
 {
-    int n, i;
-    bool isPrime = true;
-    std::cout << "Please enter an integer ";
-    std::cin >> n;
-    std::cout << n << std::endl;
-    for (i = 2; i <= (sqrt(abs(n))); i++)
-    {
-        if (n % i == 0) 
-        {
-            isPrime = false;
-            break;
-        }
-    }
-    if (isPrime)
-        std::cout << "This is a prime number" << std::endl;
-    else
-        std::cout << "This is a complex number" << std::endl;
-    return 0;
+	int n, i;
+	bool isPrime = true;
+
+	std::cout << "Please enter an integer ";
+	std::cin >> n;
+	std::cout << n << std::endl;
+
+	for (i = 2; i <= (std::sqrt(std::abs(n))); i++)
+	{
+		if (n % i == 0)
+		{
+			isPrime = false;
+			break;
+		}
+	}
+
+	if (isPrime)
+	{
+		std::cout << "This is a prime number" << std::endl;
+	}
+	else
+	{
+		std::cout << "This is a complex number" << std::endl;
+	}
+
+	return 0;
 }
