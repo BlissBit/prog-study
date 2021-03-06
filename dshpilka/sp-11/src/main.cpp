01426666cda6614088cc369d855ebf96737a61e9
DShpilkA <darkangel77792.e30@gmail.com>
2021-03-07 01:47:24 +0300

    [SP-11] добавил задачу, которая вычисляет является ли натуральное число простым

---
+++ dshpilka/sp-11/src/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int main() 
+{
+    int n, i;
+    bool isPrime = true;
+    std::cout << "Please enter an integer ";
+    std::cin >> n;
+    std::cout << n << std::endl;
+    for (i = 2; i <= (sqrt(abs(n))); i++)
+    {
+        if (n % i == 0) 
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+        std::cout << "This is a prime number" << std::endl;
+    else
+        std::cout << "This is a complex number" << std::endl;
+    return 0;
+}
