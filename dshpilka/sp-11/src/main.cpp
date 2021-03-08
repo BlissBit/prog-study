157e250cb681e5bf2699f54cef03477e7df25a1d
DShpilkA <darkangel77792.e30@gmail.com>
2021-03-08 21:42:11 +0300

    [SP-11] исправил объявление переменных и добавил проверку на ввод символов

--- dshpilka/sp-11/src/main.cpp
+++ dshpilka/sp-11/src/main.cpp
@@ -3,22 +3,28 @@
 
 int main()
 {
-	int n, i;
+	int (n);
 	bool isPrime = true;
 
 	std::cout << "Please enter an integer ";
 	std::cin >> n;
-	std::cout << n << std::endl;
-
-	for (i = 2; i <= (std::sqrt(std::abs(n))); i++)
 	{
-		if (n % i == 0)
+		while (!(std::cin >> n))
 		{
 			isPrime = false;
 			break;
 		}
-	}
+		int(i);
 
+		for (i = 2; i <= (std::sqrt(std::abs(n))); i++)
+		{
+			if (n % i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+	}
 	if (isPrime)
 	{
 		std::cout << "This is a prime number" << std::endl;
