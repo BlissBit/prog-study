207016aac4cce0e6d3566f183d9362b3c06a7c9e
AndrewBuhanov <andrew.buhanov@gmail.com>
2021-03-09 12:42:40 +0300

    [SP-7] added a program that outputs to the user all the numbers from 50 to 100 inclusive, when divided by 7, the remainder is equal to 1,2 or 5

---
+++ abuhanov/sp-7/src/division.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    for (int i = 50; i <= 100; i++)
+        if ((i % 7 == 1) || (i % 7 == 2) || (i % 7 == 5)) 
+            cout << i << " ";
+    return 0;
+}
