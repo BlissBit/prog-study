fb3b3610e306d95fc629dd3e3613c49a2365e5dc
AndrewBuhanov <andrew.buhanov@gmail.com>
2021-03-09 13:14:11 +0300

    Update division.cpp

--- abuhanov/sp-7/src/division.cpp
+++ abuhanov/sp-7/src/division.cpp
@@ -4,8 +4,10 @@ using namespace std;
  
 int main()
 {
-    for (int i = 50; i <= 100; i++)
+   { 
+       for (int i = 50; i <= 100; i++)
         if ((i % 7 == 1) || (i % 7 == 2) || (i % 7 == 5)) 
             cout << i << " ";
-    return 0;
+    }
+return 0;
 }
