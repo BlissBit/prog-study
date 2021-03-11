1990d26d90e39e1a2bd8995595051283c8f8850d
matyushov.a.a@gmail.com <matyushov.a.a@gmail.com>
2021-03-11 22:50:50 +0300

    add example input output

---
+++ amatyushov/cpp-base/life-example-includes/src/triangle.hpp
@@ -0,0 +1,9 @@
+#pragma once
+namespace Triangle
+{
+double baseAndHeight(double base, double height);
+double twoSidesAndTheAngleBetween(double side_1, double side_2, double alpha);
+double alongRadiusInscribedCircleAndThreeSides(double side_1, double side_2, double side_3, double radius);
+double alongRadiusCircumscribedCircleAndThreeSides(double side_1, double side_2, double side_3, double radius);
+}
+
