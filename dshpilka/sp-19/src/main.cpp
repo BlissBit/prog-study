6398987b3c5efce5cfae49380962fd8fcdc21f8d
DShpilkA <darkangel77792.e30@gmail.com>
2021-03-16 20:38:42 +0300

    Update main.cpp

--- dshpilka/sp-19/src/main.cpp
+++ dshpilka/sp-19/src/main.cpp
@@ -1,132 +1,44 @@
 #include <iostream>
-
+#include <string>
 int main()
 {
-	int number{}, a{}, b{}, c{}, d{}, e{}, f{}, g{}, h{};
+	int size_of_number{}, number{};
 
 	std::cout << "Enter your number: ";
 	std::cin >> number;
 
-	if (number > 0 && number < 10)
+	std::string str_number = std::to_string(number);
+	str_number.reserve();
+	int reverse{};
+	reverse = std::stoi(str_number);
+	
+	for (int i = 1; i < 100; ++i)
 	{
-		std::cout << "Your number is a palindrome" << std::endl;
+		int current_div = pow(10, i);
+		if ((number % current_div) == number)
+		{
+			size_of_number = i;
+			break;
+		}
 	}
 
-	else if (number >= 10 && number < 100)
+	reverse = 0;
+	int temp_number = number;
+	for (int i = 1; i <= size_of_number; ++i)
 	{
-		a = number / 10;
-		b = number % 10;
-		if (a == b)
+		reverse += (temp_number % 10);
+		temp_number -= (temp_number % 10);
+		temp_number /= 10;
+		if (i == size_of_number)
 		{
-			std::cout << "Your number is a palindrome" <<std::endl;
+			break;
 		}
-		else
-		{
-			std::cout << "Your number is not a palindrome" << std::endl;
-		}
+		reverse *= 10;
 	}
-	else if (number >= 100 && number < 1000)
+
+	if (reverse == number)
 	{
-		a = number / 100;
-		b = number % 100 / 10;
-		c = number % 10;
-		if (a == c)
-		{
-			std::cout << "Your number is a palindrome" << std::endl;
-		}
-		else
-		{
-			std::cout << "Your number is not a palindrome" << std::endl;
-		}
-	}
-	else if (number >= 1000 && number < 10000)
-	{
-		a = number / 1000;
-		b = number % 1000 / 100;
-		c = number % 100 / 10;
-		d = number % 10;
-		if (a == d)
-		{
-			std::cout << "Your number is a palindrome" << std::endl;
-		}
-		else
-		{
-			std::cout << "Your number is not a palindrome" << std::endl;
-		}
-	}
-	else if (number >= 10000 && number < 100000)
-	{
-		a = number / 10000;
-		b = number % 10000 / 1000;
-		c = number % 1000 / 100;
-		d = number % 100 / 10;
-		e = number % 10;
-		if (a == e)
-		{
-			std::cout << "Your number is a palindrome" << std::endl;
-		}
-		else
-		{
-			std::cout << "Your number is not a palindrome" << std::endl;
-		}
-	}
-	else if (number >= 100000 && number < 1000000)
-	{
-		a = number / 100000;
-		b = number % 100000 / 10000;
-		c = number % 10000 / 1000;
-		d = number % 1000 / 100;
-		e = number % 100 / 10;
-		f = number % 10;
-		if (a == f)
-		{
-			std::cout << "Your number is a palindrome" << std::endl;
-		}
-		else
-		{
-			std::cout << "Your number is not a palindrome" << std::endl;
-		}
-	}
-	else if (number >= 1000000 && number < 10000000)
-	{
-		a = number / 1000000;
-		b = number % 1000000 / 100000;
-		c = number % 100000 / 10000;
-		d = number % 10000 / 1000;
-		e = number % 1000 / 100;
-		f = number % 100 / 10;
-		g = number % 10;
-		if (a == g)
-		{
-			std::cout << "Your number is a palindrome" << std::endl;
-		}
-		else
-		{
-			std::cout << "Your number is not a palindrome" << std::endl;
-		}
-	}
-	else if (number >= 10000000 && number < 100000000)
-	{
-		a = number / 10000000;
-		b = number % 10000000 / 1000000;
-		c = number % 1000000 / 100000;
-		d = number % 100000 / 10000;
-		e = number % 10000 / 1000;
-		f = number % 1000 / 100;
-		g = number % 100/ 10;
-		h = number % 10;
-		if (a == h)
-		{
-			std::cout << "Your number is a palindrome" << std::endl;
-		}
-		else
-		{
-			std::cout << "Your number is not a palindrome" << std::endl;
-		}
-	}
-	else
-	{
-		std::cout << "Oops, something went wrong!" << std::endl;
+		std::cout << number << " is palindrom" << std::endl;
 	}
 
 	return 0;
