67b4e2fc3d4970589d9d669ff77ba09fafd156ba
matyushov.a.a@gmail.com <matyushov.a.a@gmail.com>
2021-03-23 22:24:26 +0300

    add functions 2 and structs

---
+++ amatyushov/cpp-base/example-functions-2/src/main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include "task_3.hpp"
+
+/// <summary>
+/// даны два целы одно больше другого перечислить от одного до другого включительно 
+/// </summary>
+void task_for1()
+{
+	int a{}, b{};
+	std::cout << "Enter a: ";
+	std::cin >> a;
+	std::cout << "Enter b: ";
+	std::cin >> b;
+	if (!(a < b))
+	{
+		std::cout << "not valid data";
+	}
+
+	for (int i = a; i <=b; ++i)
+	{
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+}
+
+/// <summary>
+/// Задача на цикл if1
+/// </summary>
+void task_for2()
+{
+	int a{};
+	std::cout << "Enter a: ";
+	std::cin >> a;
+	if (a > 0)
+	{
+		a += 1;
+	}
+	std::cout << a << std::endl;
+}
+
+int main()
+{
+	task_for1();
+	task_for2();
+	task_3::example_task3();
+	return 0;
+}
---
+++ amatyushov/cpp-base/example-functions-2/src/task_3.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+
+namespace task_3
+{
+	void example_task3();
+}
