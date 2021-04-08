679bb91bb135d17ff5e6a32e8280174982afc1d3
AndrewBuhanov <andrew.buhanov@gmail.com>
2021-04-08 23:05:01 +0300

    [SP-46]-Added programm that prompts the user to guess a number from 0 to 100 (in 10 attempts)

---
+++ abuhanov/sp-46/src/random-number.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include <ctime>
+#include <cstdlib>
+
+int main()
+{
+    int n{};
+    std::srand(static_cast<unsigned int>(time(0)));
+    std::cout<<"Guess The Number :) "<<std::endl;
+    n = (std::rand() % 100) + 1;
+    int answer{};
+    do
+    {
+        std::cout<<"Enter envisioned number "<<std::endl;
+        std::cin>>answer;
+            if (n>answer) 
+            {
+                std::cout<<"The number is bigger than you enter "<<std::endl;
+            }
+            else if (n<answer) 
+            {
+                std::cout<<"The number is smaller than you enter "<<std::endl;
+            }
+    }
+    while (n != answer);
+    std::cout<<"All right ! "<<std::endl;
+  return 0;
+}
