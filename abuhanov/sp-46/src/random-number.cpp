a73dc5e9dd8a2ef84f7c0c521a419213a30e1768
AndrewBuhanov <andrew.buhanov@gmail.com>
2021-04-10 00:14:24 +0300

    [SP-46] Added programm which prompts user to guess the number
    Programm prompts user to guess the number in 10 attempts.

--- abuhanov/sp-46/src/random-number.cpp
+++ abuhanov/sp-46/src/random-number.cpp
@@ -5,24 +5,24 @@
 int main()
 {
     int n{};
-    std::srand(static_cast<unsigned int>(time(0)));
-    std::cout<<"Guess The Number :) "<<std::endl;
+    std::srand (static_cast<unsigned int>(time(0)));
+    std::cout<<"Guess The Number :) " << std::endl;
     n = (std::rand() % 100) + 1;
     int answer{};
     do
     {
-        std::cout<<"Enter envisioned number "<<std::endl;
-        std::cin>>answer;
-            if (n>answer) 
+        std::cout << "Enter envisioned number " << std::endl;
+        std::cin >> answer;
+            if (n > answer) 
             {
-                std::cout<<"The number is bigger than you enter "<<std::endl;
+                std::cout << "The number is bigger than you enter " << std::endl;
             }
-            else if (n<answer) 
+            else if (n < answer) 
             {
-                std::cout<<"The number is smaller than you enter "<<std::endl;
+                std::cout << "The number is smaller than you enter " << std::endl;
             }
     }
     while (n != answer);
-    std::cout<<"All right ! "<<std::endl;
+    std::cout << "You guessed ! " << std::endl;
   return 0;
 }
