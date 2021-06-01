756133521ea552006db1b12d215868b6239bc54e
jackee67rus <58482990+jackee67rus@users.noreply.github.com>
2021-06-01 21:29:12 +0300

    [SP-8] исправил задачу на произведение чисел

--- skovalenko/sp-8/src/proizvedenie.cpp
+++ skovalenko/sp-8/src/proizvedenie.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
+using namespace std;
 
- int main()
+int main()
 {
-	int a;
-	std::cout << "Enter a: ";
-	std::cin >> a;
-	int res = (a % 10) * (a / 10);
-	std::cout << "Result: " << res;
-	std::cin.get();
+	int a, b;
+	cout << "Enter A:" << endl;
+	cin >> a;
+	cout  << "Enter B:" << endl;
+	cin >> b;
+	if (a < 10 || b < 10)
+	{
+		cout << "Noob! Enter A and B > || = 10" << endl;
+	}
+	else if ((a * b) > 9999999)
+	{
+		cout << "OMG OMG OMG" << endl;
+		system("pause");
+	}
+	cout << "Result: " << a * b << endl;
 	return 0;
 }
-// Создайте программу, которая находит произведение цифр введённого числа, Минимальное число двузначное, максимальное 7 значное
-// Хз как задать мин. и макс. значение, гуглил, но для c++ не нашёл ничего толкового
