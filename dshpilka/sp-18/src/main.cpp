f2f544d8c47d7dc5d75777a6beac1f08bffd9b64
Денис Шпилька <79533360+DarkAngel77792@users.noreply.github.com>
2021-06-08 15:39:02 +0300

    [SP-18] перезалил задачу на проверку принадлежности числа к последовательности Фибоначчи

---
+++ dshpilka/sp-18/src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+	int n{}, i{}, a{}, b{}, c{};
+	bool q = 0;
+
+	std::cout << "Enter n = ";
+	std::cin >> n;
+
+	a = b = c = 1;
+
+	for (i = 1; i < n; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		if (c == n)
+		{
+			q = 1;
+		}
+	}
+	if (q)
+	{
+		std::cout << "This is the Fibonacci number" << std::endl;
+	}
+	else
+	{
+		std::cout << "This is not the Fibonacci number" << std::endl;
+	}
+
+	return 0;
+}
