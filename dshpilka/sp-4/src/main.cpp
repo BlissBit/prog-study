fe1ee45b318dbd9a7a5f9fcf461f361132e81232
Денис Шпилька <79533360+DarkAngel77792@users.noreply.github.com>
2021-06-08 15:28:30 +0300

    [SP-4] перезалил задачу на проверку кратности

---
+++ dshpilka/sp-4/src/main.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int SIZE = 100;
+	int arr[SIZE]{}, i{}, n{};
+
+	for (i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+
+	for (i = 0; i < SIZE; i++)
+	{
+		std::cout << arr[i] << std::endl;
+	}
+
+	std::cout << "Kratno 2" << std::endl;
+	for (int number : arr)
+	{
+		if (number % 2 == 0)
+		{
+			std::cout << number << ' ';
+		}
+	}
+	std::cout << '\n';
+
+	std::cout << "Kratno 3" << std::endl;
+	for (int number : arr)
+	{
+		if (number % 3 == 0)
+		{
+			std::cout << number << ' ';
+		}
+	}
+	std::cout << '\n';
+
+	std::cout << "Kratno 4" << std::endl;
+	for (int number : arr)
+	{
+		if (number % 4 == 0)
+		{
+			std::cout << number << ' ';
+		}
+	}
+	std::cout << '\n';
+
+	std::cout << "Kratno 5" << std::endl;
+	for (int number : arr)
+	{
+		if (number % 5 == 0)
+		{
+			std::cout << number << ' ';
+		}
+	}
+	std::cout << '\n';
+
+	return 0;
+}
