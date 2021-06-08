9dcf8d1c9ba01f57ef4a92829d8f84976497f68b
daltonya <lildaltonya67@gmail.com>
2021-06-09 02:41:57 +0300

    Update main.cpp

--- dlobuzov/sp-47/src/main.cpp
+++ dlobuzov/sp-47/src/main.cpp
@@ -3,14 +3,17 @@ using namespace std;
 int main()
 {
     int num;
-    cout << "Enter number: ";
-    cin >> num;
-    for (int i = 1; i < 21; i++)
     {
-        cout << num;
-        cout << " * ";
-        cout << i;
-        cout << " = ";
-        cout << num * i << endl;
+        cout << "Enter number: ";
+        cin >> num;
+        for (int i = 1; i <= 20; i++)
+        {
+            cout << num;
+            cout << " * ";
+            cout << i;
+            cout << " = ";
+            cout << num * i << endl;
+        }
     }
+    return 0;
 }
