e7b89cd965f63159d69bd0422bda53243e9a1407
Денис Шпилька <79533360+DarkAngel77792@users.noreply.github.com>
2021-07-15 15:13:19 +0300

    [SP-23] добавил задачу на нахождение среднего арифметического

--- dshpilka/sp-23/src/main.cpp
+++ dshpilka/sp-23/src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	int size{};
+	int size{}, sum{};
 
 	std::cout << "Enter the number of elements from 25 to 100 = ";
 	std::cin >> size;
@@ -20,7 +20,28 @@ int main()
 		std::cout << arr[i] << std::endl;
 	}
 
+	std::cout << std::endl;
 
+	for (int m = 0; m < size; m++)
+	{
+		std::cout << arr[m] << std::endl;
+
+		for (int i = m + 1; i < size; i++)
+		{
+			if (arr[m] == arr[i])
+			{
+				for (int k = i; k < size - 1; k++)
+				{
+					arr[k] = arr[k + 1];
+				}
+				size--;
+			}
+		}
+		sum += arr[m];
+	}
+
+	std::cout << "Sum = " << sum << std::endl;
+	std::cout << "Average = " << double (sum) / double (size) << std::endl;
 
 	delete[] arr;
 
